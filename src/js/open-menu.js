572c0a4c4ea8df06eb45655f01e079bd049bba28
jacobSalazku <Cooljss10@gmail.com>
2025-06-20 08:01:54 +0200

    Add menu toggle functionality with event listener for hamburger icon

--- src/js/open-menu.js
+++ src/js/open-menu.js
@@ -1,11 +1,13 @@
 const handleMenuToggle = () => {
   const hamburger = document.getElementById('hamburger');
   const nav = document.querySelector('.nav');
+  const body = document.body;
 
   if (hamburger && nav) {
     hamburger.addEventListener('click', () => {
       nav.classList.toggle('active');
       document.getElementById('hamburger').classList.toggle('active');
+      body.classList.toggle('no-scroll');
       console.log('Clicked hamburger menu');
     });
   }
