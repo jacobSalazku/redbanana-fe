6304604d8c8f744f31ae7371caf955ddb7f70db6
jacobSalazku <Cooljss10@gmail.com>
2025-06-20 08:05:39 +0200

    feat: update webpack configuration to include CSS minimization and clean output directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const config = {
   mode: 'development',
   entry: './src/js/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   devServer: {
     open: true,
@@ -25,6 +26,9 @@ const config = {
         type: 'asset',
       },
       {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+      },
+      {
         test: /\.html$/i,
         use: ['html-loader'],
       },
@@ -32,11 +36,15 @@ const config = {
         test: /\.s[ac]ss$/i,
         use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin()],
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
   },
 };
 
