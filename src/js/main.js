aa22fefce4d824abbb9729f411ff78269e72791c
jacobSalazku <Cooljss10@gmail.com>
2025-06-20 08:04:38 +0200

    feat: refactor main.js imports and add product card styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
 import '../sass/main.scss';
-import { handleMenuToggle } from './openMenu.js';
+import { handleMenuToggle } from './open-menu.js';
+import { ProductSlider } from './product-swiper.js';
+import { sectionAnimations } from './section-anitmation.js';
 
+sectionAnimations();
 handleMenuToggle();
+ProductSlider('.product__slider');
+ProductSlider('.reviews__slider');
+ProductSlider('.blog__slider');
